10fcb4e298a59088458bbae4e9338ab1b5e9a3ba
GadzhievPavel <gadzhievpasha@gmail.com>
2023-06-21 00:14:35 +0300

    add form create user

--- src/main/java/org/example/controller/EmployeeController.java
+++ src/main/java/org/example/controller/EmployeeController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -25,8 +27,19 @@ public class EmployeeController {
     }
 
     @PostMapping(path = "/employees", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity createEmployee(@RequestBody Employee employee) {
+    public ResponseEntity createEmployee(@ModelAttribute Employee employee) {
         employeeRepository.save(employee);
         return ResponseEntity.ok().body("add");
+    }
+
+    @GetMapping(path = "/employees_page")
+    public ModelAndView getEmployeesPage(){
+
+        List<Employee> employees = employeeRepository.findAll();
+        ModelAndView view = new ModelAndView();
+        view.addObject("employee",new Employee());
+        view.setViewName("employees.html");
+        view.addObject("employees",employees);
+        return  view;
     }
 }
