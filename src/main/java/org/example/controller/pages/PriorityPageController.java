b5d400d2163cab7e181e2fa10b6e60b265035f75
GadzhievPavel <gadzhievpasha@gmail.com>
2023-07-05 23:59:46 +0300

    edit and delete priority

--- src/main/java/org/example/controller/pages/PriorityPageController.java
+++ src/main/java/org/example/controller/pages/PriorityPageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/pages")
-public class PriorityController {
+public class PriorityPageController {
 
     @Autowired
     private PriorityRepository priorityRepository;
@@ -32,5 +32,27 @@ public class PriorityController {
         view.addObject("priorities",priorities);
         view.addObject("priority", new Priority());
         return view;
+    }
+    @GetMapping(path = "/page_edit_priority")
+    public ModelAndView getEditPriority(@ModelAttribute Priority priority){
+        ModelAndView view = new ModelAndView();
+        view.setViewName("edit_priority.html");
+        view.addObject("priority",priority);
+        return view;
+    }
+
+    @PostMapping(path = "edit_priority")
+    public ModelAndView updatePriority(@ModelAttribute Priority priority){
+        var priorityDB =priorityRepository.findById(priority.getId()).get();
+        priorityDB.setName(priority.getName());
+        priorityDB.setValue(priority.getValue());
+        priorityRepository.save(priorityDB);
+        return getPage();
+    }
+
+    @PostMapping(path = "delete_priority")
+    public ModelAndView deletePriority(@ModelAttribute Priority priority){
+        priorityRepository.deleteById(priority.getId());
+        return getPage();
     }
 }
